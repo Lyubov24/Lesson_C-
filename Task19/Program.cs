69a6eeec2863d92edad563e45bb5259c28e6a31c
Lyubov Zhudrik <lyubov.zhudrik@gmail.com>
2022-11-20 22:36:02 +0300

    Add Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,3 +4,19 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное  число: ");
+int num  = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num % 10;
+int num11 = num / 10000;
+if (num1 == num11)
+{
+ int num2 = num / 10 % 10;
+ int num22 = num / 1000 % 10;
+ if (num2 == num22)
+ 
+Console.WriteLine($"{num} -> да");
+}
+ else Console.WriteLine($"{num} -> нет");
+
