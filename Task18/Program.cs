e7b20eb6c5544d1cdaef38ca41bb563735b0ce8d
Lyubov Zhudrik <lyubov.zhudrik@gmail.com>
2022-11-20 16:57:50 +0300

    Add Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,15 +3,15 @@
 
 
 Console.WriteLine("Введите номер четверти ");
-int quarter = Convert.ToInt32(Console.ReadLine());
+string quarter = Console.ReadLine();
 
-string Range(int quarter1)
+string Range(string quarter1)
 {
-    if (quarter1 == 1) return "x > 0 и y > 0";
-    if (quarter1 == 2) return "x < 0 и y > 0";
-    if (quarter1 == 3) return "x < 0 и y < 0";
-    if (quarter1 == 4) return "x > 0 и y < 0";
-    return "Указан неверный номер четверти";
+    if (quarter1 == "1") return "x > 0 и y > 0";
+    if (quarter1 == "2") return "x < 0 и y > 0";
+    if (quarter1 == "3") return "x < 0 и y < 0";
+    if (quarter1 == "4") return "x > 0 и y < 0";
+    return "Введены неверные данные";
 }
 string range = Range(quarter);
 Console.WriteLine(range);
