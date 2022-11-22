0e9abd578dda74367fc88147abc2cac857c23f70
Lyubov Zhudrik <lyubov.zhudrik@gmail.com>
2022-11-22 12:16:08 +0300

    add Task24, 28, 30

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,14 +5,14 @@
 
 Console.WriteLine("Введите номер дня недели: ");
 
-int number = int.Parse(Console.ReadLine()); 
-                if (number >= 1 && number <= 5) 
-                { 
-                    Console.Write("Нет"); 
-                } 
- 
-                else if (number >= 6 && number <= 7) 
-                { 
-                    Console.Write("Да"); 
-                } 
+int number = int.Parse(Console.ReadLine());
+if (number >= 1 && number <= 5)
+{
+    Console.Write("Нет");
+}
+
+else if (number >= 6 && number <= 7)
+{
+    Console.Write("Да");
+}
 else Console.WriteLine("Введены некорректные данные");
---
+++ Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число (N) и выдает произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int number = 0;
+while (number <= 0)
+{
+    Console.WriteLine("Введите число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number < 0) Console.WriteLine("Требуется ввести натуральное число");
+}
+
+int MultDigits(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+
+int multDigits = MultDigits(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {multDigits}");
