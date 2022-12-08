e592f93ff26823e0867b7269ae2dd1c35e0c5be2
Lyubov Zhudrik <lyubov.zhudrik@gmail.com>
2022-12-08 15:12:09 +0300

    add task 47, 50, 52

---
+++ Task50_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+Console.WriteLine("Введите номер столбца: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите номер строки: ");
+int j = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = CreateMatrixRndInt(5, 5, 1, 10);
+PrintMatrix(matrix);
+
+if (i < 0 || j < 0) Console.WriteLine("Введены некорректные данные");
+else
+{
+if (i < matrix.GetLength(0) && j < matrix.GetLength(1)) Console.Write($" [{i};{j}] -> {matrix[i, j]}");
+else Console.WriteLine($"[{i};{j}] -> такого числа в массиве нет");
+}
---
+++ Task52/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max) 
+{
+    int[,] matrix = new int[rows, colums]; 
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix; 
+}
+
+void PrintMatrix(int[,] matrix)    
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, "); 
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+
+double[] avgNumbers = new double[array2D.GetLength(1)];
+
+for (int i = 0; i < array2D.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < array2D.GetLength(0); j++)
+    {
+        result += array2D[j, i];
+    }
+    avgNumbers[i] = result / array2D.GetLength(0);
+}
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{Math.Round(array[i], 1) + "; " }");
+        else Console.Write($"{Math.Round(array[i], 1) + " " }");
+        
+    }
+   
+}
+ Console.WriteLine();
+PrintArray(avgNumbers);
+
